1fe102d311140d719e31c38d68b8b440e5a805c0
Denis Koshelev <truemacintoshchannel@gmail.com>
2021-10-09 16:33:02 +0200

    global configs added

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import telebot
-import local_configs as config
+# import local_configs as config
+import env_configs as config
 from search import *
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
